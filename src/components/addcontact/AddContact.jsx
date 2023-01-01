12cb4ca08c4e29acd58310e9fd438485d7fae1d3
Rihan Bagwan <rihanbagwan5017@gmail.com>
2023-01-01 16:31:38 +0530

    Done with create part

--- src/components/addcontact/AddContact.jsx
+++ src/components/addcontact/AddContact.jsx
@@ -1,22 +1,45 @@
 import React from "react";
-
+// to pass the data from parent to child we use props
 class AddContact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+  };
+  add = (e) => {
+    e.preventDefault();
+    if (this.state.name === "" || this.state.email === "") {
+      alert("All the feilds are mandatory!");
+      return;
+    }
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", email: "" });
+  };
   render() {
     return (
       <div className="ui main">
         <h2>Add Contact</h2>
-        <form className="ui from">
+        <form className="ui from" onSubmit={this.add}>
           <div className="field">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" placeholder="name" />
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
           </div>
           <div className="field">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="username@gmail.com" />
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="username@gmail.com"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
           </div>
-          <button type="button" className="ui button blue">
-            Add Contact
-          </button>
+          <button className="ui button blue">Add</button>
         </form>
       </div>
     );
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Header.jsx";
 import "./App.css";
 import AddContact from "../addcontact/AddContact";
 import ContactList from "../contactList/ContactList";
 // A component is a peice of reuseable code
+// useEffect = Whenever the value changes it help us  to render the compoenet again
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Rihan",
-      email: "rihanbagwan5017@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Saboor",
-      email: "saboorbagwan5017@gmail.com",
-    },
-  ];
+  // handlers are used to pass the data from child to parent. -
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
+  };
+  useEffect(() => {
+    const retriveContact = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContact) setContacts(retriveContact);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts = {contacts}/>
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
